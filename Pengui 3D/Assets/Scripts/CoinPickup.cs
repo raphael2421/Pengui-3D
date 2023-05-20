efc205d62b59aa3b53c4dd244b04528fd84df2b5
allangonzalezmiceli16 <allangonzalezmiceli16@gmail.com>
2023-05-20 14:14:22 -0700

    Audio Huevo, Ajuste Escenarios

--- Pengui 3D/Assets/Scripts/CoinPickup.cs
+++ Pengui 3D/Assets/Scripts/CoinPickup.cs
@@ -5,27 +5,35 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     public int value;
-
+    public AudioSource Eggaudio;
     public GameObject coinEffect;
 
-    void Start()
+    public void Awake()
     {
         
+    }
+    void Start()
+    {
+        //Eggaudio =  GetComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Eggaudio.Play();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
+            Eggaudio.Play();
             GameManager.instance.AddCoins(value);
-            Destroy(gameObject);
             Instantiate(coinEffect, transform.position, transform.rotation);
+            Destroy(gameObject);
+
+
         }
     }
 }
